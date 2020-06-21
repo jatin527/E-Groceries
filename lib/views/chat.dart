bd104aa997cfc2552c3f4aef9cc34495e270409b
Akki <akshatjain981000@gmail.com>
2020-06-21 17:26:54 +0530

    Added predefined text

--- lib/views/chat.dart
+++ lib/views/chat.dart
@@ -15,22 +15,25 @@ class Chat extends StatefulWidget {
 }
 
 class _ChatState extends State<Chat> {
-
   Stream<QuerySnapshot> chats;
   TextEditingController messageEditingController = new TextEditingController();
+  String selectedItem = '';
 
-  Widget chatMessages(){
+  Widget chatMessages() {
     return StreamBuilder(
       stream: chats,
-      builder: (context, snapshot){
-        return snapshot.hasData ?  ListView.builder(
-          itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index){
-              return MessageTile(
-                message: snapshot.data.documents[index].data["message"],
-                sendByMe: Constants.myName == snapshot.data.documents[index].data["sendBy"],
-              );
-            }) : Container();
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+                  return MessageTile(
+                    message: snapshot.data.documents[index].data["message"],
+                    sendByMe: Constants.myName ==
+                        snapshot.data.documents[index].data["sendBy"],
+                  );
+                })
+            : Container();
       },
     );
   }
@@ -39,16 +42,15 @@ class _ChatState extends State<Chat> {
     if (messageEditingController.text.isNotEmpty) {
       Map<String, dynamic> chatMessageMap = {
         "sendBy": Constants.myName,
-        "message": messageEditingController.text,
-        'time': DateTime
-            .now()
-            .millisecondsSinceEpoch,
+        "message": selectedItem + ' ' + (messageEditingController.text),
+        'time': DateTime.now().millisecondsSinceEpoch,
       };
 
       DatabaseMethods().addMessage(widget.chatRoomId, chatMessageMap);
 
       setState(() {
         messageEditingController.text = "";
+        selectedItem = '';
       });
     }
   }
@@ -63,6 +65,72 @@ class _ChatState extends State<Chat> {
     super.initState();
   }
 
+  void _onTextButtonPresed() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          color: Color(0xFF1F1F1F),
+          height: 350,
+          child: Container(
+            child: Expanded(child: _buildBottomNavigationMenu()),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: const Radius.circular(25),
+                topRight: const Radius.circular(25),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Column _buildBottomNavigationMenu() {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text('Order completed'),
+          onTap: () => _selectItem('Order completed '),
+        ),
+        ListTile(
+          title: Text('Cost of Total order ₹ '),
+          onTap: () => _selectItem('Cost of Total order ₹ '),
+        ),
+        ListTile(
+          title: Text('Delivery by'),
+          onTap: () => _selectItem('Delivery by '),
+        ),
+        ListTile(
+          title: Text('Delivery not possible. Order Take away at'),
+          onTap: () =>
+              _selectItem('Delivery not possible. Order Take away at '),
+        ),
+        ListTile(
+          title: Text('Order will be delayed '),
+          onTap: () => _selectItem('Order will be delayed '),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.close,
+          ),
+          title: Text('Clear'),
+          onTap: () => _selectItem(''),
+        ),
+      ],
+    );
+  }
+
+  void _selectItem(String name) {
+    Navigator.pop(context);
+    setState(
+      () {
+        selectedItem = name;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,53 +139,79 @@ class _ChatState extends State<Chat> {
         child: Stack(
           children: [
             chatMessages(),
-            Container(alignment: Alignment.bottomCenter,
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .width,
+            Container(
+              alignment: Alignment.bottomCenter,
+              width: MediaQuery.of(context).size.width,
               child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
                 color: Color(0x54FFFFFF),
-                child: Row(
-                  children: [
-                    Expanded(
+                child: Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
                         child: TextField(
                           controller: messageEditingController,
+                          textCapitalization: TextCapitalization.sentences,
+                          autocorrect: false,
                           style: simpleTextStyle(),
                           decoration: InputDecoration(
-                              hintText: "Message ...",
-                              hintStyle: TextStyle(
+                              prefix: Text(selectedItem),
+                              prefixStyle: TextStyle(
                                 color: Colors.white,
                                 fontSize: 16,
                               ),
-                              border: InputBorder.none
-                          ),
-                        )),
-                    SizedBox(width: 16,),
-                    GestureDetector(
-                      onTap: () {
-                        addMessage();
-                      },
-                      child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                  colors: [
-                                    const Color(0x36FFFFFF),
-                                    const Color(0x0FFFFFFF)
-                                  ],
-                                  begin: FractionalOffset.topLeft,
-                                  end: FractionalOffset.bottomRight
+                              border: InputBorder.none),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          addMessage();
+                        },
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                      colors: [
+                                        const Color(0x36FFFFFF),
+                                        const Color(0x0FFFFFFF)
+                                      ],
+                                      begin: FractionalOffset.topLeft,
+                                      end: FractionalOffset.bottomRight),
+                                  borderRadius: BorderRadius.circular(40)),
+                              padding: EdgeInsets.all(12),
+                              child: Image.asset(
+                                "assets/images/send.png",
+                                height: 25,
+                                width: 25,
                               ),
-                              borderRadius: BorderRadius.circular(40)
-                          ),
-                          padding: EdgeInsets.all(12),
-                          child: Image.asset("assets/images/send.png",
-                            height: 25, width: 25,)),
-                    ),
-                  ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Ink(
+                                decoration: const ShapeDecoration(
+                                  color: Color(0x0FFFFFFF),
+                                  shape: CircleBorder(),
+                                ),
+                                child: IconButton(
+                                  icon: Icon(
+                                    Icons.text_fields,
+                                  ),
+                                  color: Colors.white,
+                                  iconSize: 20,
+                                  onPressed: () {
+                                    _onTextButtonPresed();
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -126,7 +220,6 @@ class _ChatState extends State<Chat> {
       ),
     );
   }
-
 }
 
 class MessageTile extends StatelessWidget {
@@ -135,50 +228,41 @@ class MessageTile extends StatelessWidget {
 
   MessageTile({@required this.message, @required this.sendByMe});
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(
-          top: 8,
-          bottom: 8,
-          left: sendByMe ? 0 : 24,
-          right: sendByMe ? 24 : 0),
+          top: 8, bottom: 8, left: sendByMe ? 0 : 24, right: sendByMe ? 24 : 0),
       alignment: sendByMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
-        margin: sendByMe
-            ? EdgeInsets.only(left: 30)
-            : EdgeInsets.only(right: 30),
-        padding: EdgeInsets.only(
-            top: 17, bottom: 17, left: 20, right: 20),
+        margin:
+            sendByMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
+        padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
         decoration: BoxDecoration(
-            borderRadius: sendByMe ? BorderRadius.only(
-                topLeft: Radius.circular(23),
-                topRight: Radius.circular(23),
-                bottomLeft: Radius.circular(23)
-            ) :
-            BorderRadius.only(
-        topLeft: Radius.circular(23),
-          topRight: Radius.circular(23),
-          bottomRight: Radius.circular(23)),
-            gradient: LinearGradient(
-              colors: sendByMe ? [
-                const Color(0xff007EF4),
-                const Color(0xff2A75BC)
-              ]
-                  : [
-                const Color(0x1AFFFFFF),
-                const Color(0x1AFFFFFF)
-              ],
-            )
+          borderRadius: sendByMe
+              ? BorderRadius.only(
+                  topLeft: Radius.circular(23),
+                  topRight: Radius.circular(23),
+                  bottomLeft: Radius.circular(23))
+              : BorderRadius.only(
+                  topLeft: Radius.circular(23),
+                  topRight: Radius.circular(23),
+                  bottomRight: Radius.circular(23)),
+          gradient: LinearGradient(
+            colors: sendByMe
+                ? [const Color(0xff007EF4), const Color(0xff2A75BC)]
+                : [const Color(0x1AFFFFFF), const Color(0x1AFFFFFF)],
+          ),
         ),
-        child: Text(message,
-            textAlign: TextAlign.start,
-            style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontFamily: 'OverpassRegular',
-            fontWeight: FontWeight.w300)),
+        child: Text(
+          message,
+          textAlign: TextAlign.start,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontFamily: 'OverpassRegular',
+              fontWeight: FontWeight.w300),
+        ),
       ),
     );
   }
