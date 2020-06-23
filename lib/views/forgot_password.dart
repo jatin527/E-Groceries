193bf4df21fe7a788e750c334e463fbc088d5d88
jatin527 <jatinsingal2@gmail.com>
2020-06-23 18:05:57 +0530

    Update forgot_password.dart

--- lib/views/forgot_password.dart
+++ lib/views/forgot_password.dart
@@ -13,15 +13,17 @@ AuthService authService = new AuthService();
 class _ForgotPasswordState extends State<ForgotPassword> {
   TextEditingController useremailEditingController =
       new TextEditingController();
-  bool isLoading = false;
   bool visible = false;
+  bool visible1 = false;
   bool isEmail = false;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   forgetpass() async {
-    
-      await authService.resetPass(useremailEditingController.text);
-    
+    if (formKey.currentState.validate()) {
+      await authService.resetPass(useremailEditingController.text).then((result){
+        print(result);
+      });
+    }
   }
 
   @override
@@ -29,81 +31,92 @@ class _ForgotPasswordState extends State<ForgotPassword> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: appBarMain(context),
-      body: isLoading
-          ? Container(
-              child: Center(
-                child: CircularProgressIndicator(),
+      body: Container(
+        padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 55.0),
+        child: Form(
+          key: formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                validator: (val) {
+                  if (!EmailValidator.validate(val)) {
+                    setState(() {
+                      visible1 = true;
+                      visible = false;
+                    });
+
+                    return 'Please enter a valid email';
+                  } else {
+                    setState(() {
+                      isEmail = true;
+                      visible = true;
+                      visible1 = false;
+                    });
+                  }
+                },
+                style: simpleTextStyle(),
+                controller: useremailEditingController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: textFieldInputDecoration("Enter your E-mail",
+                    iconic: Icons.mail),
               ),
-            )
-          : Container(
-              padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 55.0),
-              child: Form(
-                child: ListView(
-                  children: [
-                    TextFormField(
-                      style: simpleTextStyle(),
-                      controller: useremailEditingController,
-                      validator: (val){
-                        if (!EmailValidator.validate(val)) {
-                          return 'Please enter a valid email';
-                        } else {
-                          setState(() {
-                            isEmail = true;
-                          });
-                        }
-                      },
-                      decoration: textFieldInputDecoration("Enter your E-mail",
-                          iconic: Icons.mail),
-                    ),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    visible
-                        ? new Row(
-                            children: <Widget>[
-                              Text(
-                                "Mail Sent. Please Check your inbox",
-                                textAlign: TextAlign.center,
-                                style: simpleTextStyle(),
-                              )
-                            ],
-                          )
-                        : new Container(),
-                        
-                    SizedBox(
-                      height: 25,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                          forgetpass();
-                          setState(() {
-                            visible = true;
-                          });
-                        
-                      },
-                      child: Container(
-                        padding: EdgeInsets.symmetric(vertical: 16),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            gradient: LinearGradient(
-                              colors: [
-                                const Color(0xff007EF4),
-                                const Color(0xff2A75BC)
-                              ],
-                            )),
-                        width: MediaQuery.of(context).size.width,
-                        child: Text(
-                          "Reset Password",
-                          style: biggerTextStyle(),
+              SizedBox(
+                height: 25,
+              ),
+              visible
+                  ? new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Mail Sent\nPlease Check your inbox",
                           textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  ],
+                          style: simpleTextStyle(),
+                        )
+                      ],
+                    )
+                  : new Container(),
+              visible1
+                  ? new Row(
+                    
+                    mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Mail not Sent\nPlease check you email id",
+                          textAlign: TextAlign.center,
+                          style: simpleTextStyle(),
+                        )
+                      ],
+                    )
+                  : new Container(),
+              SizedBox(
+                height: 25,
+              ),
+              GestureDetector(
+                onTap: () {
+                  forgetpass();
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      gradient: LinearGradient(
+                        colors: [
+                          const Color(0xff007EF4),
+                          const Color(0xff2A75BC)
+                        ],
+                      )),
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    "Reset Password",
+                    style: biggerTextStyle(),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
-            ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
